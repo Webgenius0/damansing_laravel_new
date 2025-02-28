124d825bcfda01620a7792faa5bd5e007a19e7ad
unknown <alauddinnajim202@gmail.com>
2025-02-28 12:30:11 +0600

    update CORS configuration to specify allowed origins and enable credentials

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        env('FRONTEND_URL'),
+        'http://localhost:5173',
+        'https://damansingh.softvencefsd.xyz'
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +33,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
