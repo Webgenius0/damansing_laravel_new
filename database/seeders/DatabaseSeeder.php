485e90f91e5d349c4780f9ca655ab47cc1d72bc7
jalis <jalismahamud31@gmail.com>
2025-02-28 17:59:37 +0600

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(CmsSeeder::class);
+        $this->call(SystemSettingSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(PromoCodeSeeder::class);
