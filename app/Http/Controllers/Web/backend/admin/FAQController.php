c9fe72c6527b0f3b7d31ce3b61f8fe0665717578
jalis <jalismahamud31@gmail.com>
2025-03-03 15:14:02 +0600

    Update and push

--- app/Http/Controllers/Web/backend/admin/FAQController.php
+++ app/Http/Controllers/Web/backend/admin/FAQController.php
@@ -57,9 +57,7 @@ class FAQController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+ 
     public function create()
     {
         $faq = Cms::where('page', 'faq')->where('section', 'faqSection')->first();
